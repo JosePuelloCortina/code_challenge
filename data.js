bde35a6da5d1c901a3012c16c5cb22fca72f40dc
JosePuelloCortina <puelloj66@gmail.com>
2023-03-24 14:33:47 -0500

    completing test

---
+++ data.js
@@ -0,0 +1,22 @@
+ module.exports= domainsList = `*.amazon.co.uk 89
+    *.doubleclick.net 139
+    *.fbcdn.net 212
+    *.in-addr.arpa 384
+    *.l.google.com 317
+    1.client-channel.google.com 110
+    6.client-channel.google.com 45
+    a.root-servers.net 1059
+    apis.google.com 43
+    clients4.google.com 71
+    clients6.google.com 81
+    connect.facebook.net 68
+    edgemqtt.facebook.com 56
+    graph.facebook.com 150
+    mail.google.com 128
+    mqttmini.facebook.com 47
+    ssl.google-analytics.com 398
+    star-mini.c10r.facebook.com 46
+    staticxx.facebook.com 48
+    www.facebook.com 178
+    www.google.com 162
+    www.googleanalytics.com 127`;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,54 @@
 import { Router, Request, Response } from "express";
-let file = require('../../data.json')
+let domainsList = require('../../data.js')
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-    const dataArr = new Set(file.data);
-    let result = [...dataArr]
-    function contadorElemento (dataArr:any, busqueda:any) {
-        let acumulador = 0;
-        for (let i = 0; i<dataArr.length; i++) {
-            if (dataArr[i] === busqueda) {
-                acumulador++;
+
+    function countDomains(logs: string, minHits: number = 0): string{
+        const domainCounts: { [domain: string]: number } = {};
+        const lines = logs.split('\n');
+        
+
+        for (let line of lines) {
+            const [ domain, countStr] = line.trim().split(/\s+/);
+            const count = parseInt(countStr);
+
+            const domainParts = domain.split('.');
+            const domainLength = domainParts.length;
+            const topLevelDomian = domainParts[domainLength - 1];
+            const secondLevelDomian = domainParts[domainLength - 2];
+
+            if ( topLevelDomian === 'com' || topLevelDomian === 'co'){
+                const mainDomain = `${domainParts[domainLength - 3]}.${secondLevelDomian}.${topLevelDomian}`;
+                domainCounts[mainDomain] = (domainCounts[mainDomain] || 0) + count;
+            }else{
+                const mainDomain = `${secondLevelDomian}.${topLevelDomian}`;
+                domainCounts[mainDomain] = (domainCounts[mainDomain] || 0) + count;
             }
-        } 
-        return acumulador;
+        }
+
+
+        const domainCountsArray = Object.entries(domainCounts).map(([domain, count]) => ({ domain, count }));
+      
+
+        domainCountsArray.sort((a, b) => {
+          if (a.count === b.count) {
+            return a.domain.localeCompare(b.domain);
+          } else {
+            return b.count - a.count;
+          }
+        });
+        const filteredArray = domainCountsArray.filter(({ count }) => count >= minHits);
+        const outputLines = filteredArray.map(({ domain, count }) => `${domain} (${count})`);
+
+
+        return outputLines.join('\n');
       }
-      const domains = new Array<any>();
-      result.forEach(function(d, index){
-        domains.push(d + "(" +contadorElemento(file.data, d) + ")");
-      })
- 
-    res.send(domains);
+
+    const result = countDomains(domainsList, 50);
+    res.send(result);
+
 })
 
 export default router;
